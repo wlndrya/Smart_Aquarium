0dad4137f0f096d526ac86a2a599e62c0f0db75a
Yulia Wulandari <yuliawulandari271@gmail.com>
2021-03-31 15:01:12 +0700

    terupdate guis

--- app/src/main/java/com/example/smartaquarium/dashboard.java
+++ app/src/main/java/com/example/smartaquarium/dashboard.java
@@ -1,44 +1,64 @@
 package com.example.smartaquarium;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 import java.util.ArrayList;
 import java.util.Set;
 
-public class dashboard extends AppCompatActivity {
+public class dashboard<images> extends AppCompatActivity {
+    ImageButton androidImageButton, imageButton4, imageButton5;
 
-    ViewFlipper imageflip;
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        int images[] = {R.drawable.ikanhias, R.drawable.ikansatu, R.drawable.ikanbadut};
+        androidImageButton = (ImageButton) findViewById(R.id.imageButton2);
+        imageButton4 = (ImageButton) findViewById(R.id.imageButton4);
+        imageButton5 = (ImageButton) findViewById(R.id.imageButton5);
+        androidImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PakanIkan.class);
+                startActivity(intent);
+            }
+        });
 
-        imageflip = findViewById(R.id.imageflip);
+        imageButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), kadarair.class);
+                startActivity(intent);
+            }
+        });
 
-        //pengulangan
-        /*for (int i = 0; i < images.length; i++){
-            flipperImages(images[i]);
-        }*/
-
-        for (int image: images){
-            flipperImages(image);
-        }
+        imageButton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), gantiair.class);
+                startActivity(intent);
+            }
+        });
+    }
+//            private void openPakanIkan() {
+//
+//            }
 
 
         //ButtonMenuPakanIkan
-        button = (Button) findViewById(R.id.buttonpakan);
+        /*button = (Button) findViewById(R.id.buttonpakan);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,19 +70,6 @@ public class dashboard extends AppCompatActivity {
         public void openPakanIkan() {
             Intent intent = new Intent(this, PakanIkan.class);
             startActivity(intent);
-        }
+        }*/
 
-        //GambarSlideDashboard
-        public void flipperImages(int image){
-            ImageView imageView = new ImageView(this);
-            imageView.setBackgroundResource(image);
-
-            imageflip.addView(imageView);
-            imageflip.setFlipInterval(4000);
-            imageflip.setAutoStart(true);
-
-            //animasi
-            imageflip.setInAnimation(this, android.R.anim.slide_in_left);
-            imageflip.setOutAnimation(this, android.R.anim.slide_out_right);
-        }
-    }
+}
---
+++ app/src/main/java/com/example/smartaquarium/gantiair.java
@@ -0,0 +1,14 @@
+package com.example.smartaquarium;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class gantiair extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gantiair);
+    }
+}
